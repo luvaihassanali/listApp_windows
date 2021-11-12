22511020bbdac52f38bca4aba2e47e1a20a61c29
Luvai <luvaihassanali@gmail.com>
2021-11-11 23:41:43 -0500

    disable gpu

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace ListApp
                 //By default CefSharp will use an in-memory cache, you need to specify a Cache Folder to persist data
                 CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
             };
-
+            settings.DisableGpuAcceleration();
             Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
